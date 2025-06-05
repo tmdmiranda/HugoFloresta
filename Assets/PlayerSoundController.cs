6c0c11762bc4aab403abc6ee5bfac0c7aa52855e
Gonki04 <goncaloaraujo2004@gmail.com>
2025-06-05 10:45:43 +0100

    tou a dar commit pelo gonki nao sei o que esta ca

---
+++ Assets/PlayerSoundController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class PlayerSoundController : MonoBehaviour
+{
+    private AudioSource Walk;
+    private PlayerInputHandler inputHandler;
+
+    void Start()
+    {
+        Walk = GetComponent<AudioSource>();
+        inputHandler = GetComponent<PlayerInputHandler>();
+
+        if (Walk != null)
+        {
+            Walk.Stop();
+        }
+    }
+
+    void Update()
+    {
+        if (Walk != null && inputHandler != null)
+        {
+            // Se houver input de movimento (andar ou sprintar)
+            if (inputHandler.MovementInput.magnitude > 0.1f && (inputHandler.SprintTriggered || inputHandler.MovementInput.magnitude > 0.1f))
+            {
+                if (!Walk.isPlaying)
+                {
+                    Walk.Play();
+                }
+            }
+            else
+            {
+                if (Walk.isPlaying)
+                {
+                    Walk.Stop();
+                }
+            }
+        }
+    }
+}
